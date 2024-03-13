daa992e3c62d4af7924d0064d039bf1476076c90
Monis Akhtar <monisakhtar0503@gmail.com>
2024-03-13 15:18:16 +0100

    Created a model

--- src/components/model_builder.py
+++ src/components/model_builder.py
@@ -2,27 +2,34 @@ import torch
 from torch import nn 
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
-class CNN_model(nn.Module):
-  def __init__(self, input_shape:int, hidden_units: int, output_shape : int):
-    super().__init__()
-    self.cnn_layers = nn.Sequential(
-        nn.Conv2d(in_channels=input_shape,
-                  out_channels=hidden_units,
-                  kernel_size=3,
-                  stride = 1,
-                  padding=1),
-        nn.ReLU(),
-        nn.MaxPool2d(kernel_size=2,
-                     stride =2)
-    )
-    self.classifier = nn.Sequential(
-        nn.Flatten(),
-        nn.Linear(in_features=hidden_units *128 * 128,
-                  out_features=output_shape)
-    )
-  def forward(self, x: torch.Tensor):
-    x = self.cnn_layers(x)
-    # print(x.shape)
-    x = self.classifier(x)
-    # print(x.shape)
-    return x
+class BaseModel_0(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(input_shape, hidden_units, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(hidden_units * 64 * 64,
+                      out_features=output_shape),
+        )
+
+    def forward(self, x):
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+        # print(x.shape)
+        x = self.classifier(x)
+        return x
